352109bfe5c55be691dc2529a01aceb3598ff241
Kaitlyn O'Donnell <kodonnell327@gmail.com>
2019-04-15 07:47:56 -0400

    working on symlinks

--- storage.c
+++ storage.c
@@ -32,6 +32,7 @@ storage_stat(const char* path, struct stat* st)
 
     if (inum < 0) return inum;
     
+    
     inode* node = get_inode(inum);
 
     st->st_mode = node->mode;
@@ -92,6 +93,7 @@ storage_mknod(const char* path, int mode)
     if (rv < 0) { 
         free(dn);
         free(bn);
+        printf("mknod tree_lookup rv = %d\n", rv);
         return rv;
     }
 
@@ -101,6 +103,7 @@ storage_mknod(const char* path, int mode)
 
     free(dn);
     free(bn);
+    printf("mknod directory put = %d\n", rv);
     return rv;
 }
 
@@ -239,8 +242,15 @@ storage_rename(const char *from, const char *to)
 }
 
 int storage_set_time(const char* path, const struct timespec ts[2]) {
-    
-    return 0;
+    // struct timespec {
+    //      time_t tv_sec; // time in seconds
+    //      long int tv_nsec; // rest of elapsed time in nanoseconds
+    // }
+    // ts[0] = last access time (modern systems tend to ignore this)
+    // ts[1] = last modification time
+    int rv = clock_gettime(CLOCK_REALTIME, &ts[0]);
+    rv = clock_gettime(CLOCK_REALTIME, &ts[1]);
+    return rv;
 }
 
 int storage_symlink(const char* file, const char* link) {
@@ -248,7 +258,7 @@ int storage_symlink(const char* file, const char* link) {
     if (file_inum < 0) return file_inum;
     
     // make the symbolic link
-    int rv = storage_mknod(link, 012755); // are these the right permissions?
+    int rv = storage_mknod(link, 120000); // are these the right permissions?
     if (rv < 0) return rv;
     
     int link_inum = tree_lookup(link);
@@ -256,40 +266,33 @@ int storage_symlink(const char* file, const char* link) {
 
     inode* link_inode = get_inode(link_inum);
     inode* file_inode = get_inode(file_inum);
-    // copy data ptrs
-    // FIXME: this data could be overwritten if we remove the original inode
-    link_inode->ptrs[0] = file_inode->ptrs[0];
-    link_inode->ptrs[1] = file_inode->ptrs[1];
-    link_inode->iptr = file_inode->iptr;
+    // write the path to the data of this link
+    // the path should never be greater than a page
+    
+    int path_length = strlen(file);
+    link_inode->ptrs[0] = alloc_page();
+    link_inode->size = path_length;
+    char* page = (char*) pages_get_page(link_inode->ptrs[0]);
+    strcpy(page, file, strlen(file));  
     // FIXME: fuse is getting an io error for some reason
     return 0;
 }
 
-//TODO
 // reads the contents of a symbolic link
-int storage_readlink(const char* restrict path, char* restrict buf, size_t bufsize) {
-    
-    // how is this different than reading normally?
-    int rv = storage_read(path, buf, bufsize, 0);
+int storage_readlink(const char* restrict path, char* restrict buf, size_t bufsize) {    
+    // make sure bufsize is positive
+    if (bufsize < 0) return EINVAL;
 
-    // returns the size of the buf written
-    return rv;
+    // read the path from the link
+    int link_inum = tree_lookup(link);
+    if (link_inum < 0) return link_inum;
 
-    /*
-    FROM STORAGE_READ 
-        int inum = tree_lookup(path);
-        if (inum < 0) {
-            return inum;
-        }
+    inode* link_inode = get_inode(link_inum);
 
-        inode* node = get_inode(inum);
-        char* page = pages_get_page(node->ptrs[0]);
+    // check the mode to make sure this is a link
+    if (link_inode->mode & 012000) return EINVAL; 
 
-        //TODO: ASSUMING LESS THAN 4k
-        // depending on the size, you would need to get
-        // multiple pages from the inode to read
-        memcpy(buf, page + offset, size);
-
-        return size;
-    */
+    char* page = (char*) pages_get_page(link_inode->ptrs[0]);
+    printf("readlink reads the link as: %s\n", page);
+    return storage_read(page, buf, bufsize, 0);
 }
